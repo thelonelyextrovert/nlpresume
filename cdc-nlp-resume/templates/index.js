11349a42ccab67386942b00c89cf0d59e760629a
thelonelyextrovert <48013139+thelonelyextrovert@users.noreply.github.com>
2021-04-17 12:26:47 +0530

    Delete cdc-nlp-resume directory

--- cdc-nlp-resume/templates/index.js
+++
@@ -1,7 +0,0 @@
-
-// upload_button = document.getElementById()
-function upload_function()
-{
-    var i = document.getElementById("input_file").value;
-    console.log(i);
-}
--- cdc-nlp-resume/test.py
+++
@@ -1,9 +0,0 @@
-import pymongo
-from pymongo import MongoClient
-
-cluster = MongoClient("mongodb+srv://surya_mongodb:P3ass57word@cluster0.6ns8n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-
-db = cluster["resume"]
-collection = db["resume"]
-
-results = collection.remove({})
